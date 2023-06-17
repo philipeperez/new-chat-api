c2f8b7f6186b33670c68fbc9bf25d97926aca96b
philipeperez <philipeperez@gmail.com>
2023-06-17 13:46:31 -0300

    Logs

--- src/app.js
+++ src/app.js
@@ -9,12 +9,14 @@ io.on('connection', socket => {
 
     socket.on('joinRoom', roomName => {
         socket.join(roomName)
+        console.log(`Cliente ${socket.id} entrou na sala ${roomName}`)
         socket.on('message', msg => {
             io.to(roomName).emit('message', msg)
         })
     })
     socket.on('leaveRoom', roomName => {
         socket.leave(roomName)
+        console.log(`Cliente ${socket.id} saiu da sala ${roomName}`)
     })
 })
 
