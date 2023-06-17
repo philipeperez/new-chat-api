c209d5554633bee4de2135783ad7255af537debd
philipeperez <philipeperez@gmail.com>
2023-06-17 12:08:30 -0300

    First Commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import {Server} from 'socket.io'
+
+const PORT = 4000
+
+const io = new Server(PORT)
+
+io.on('connection', socket => {
+    console.log('Client connected', socket.id)
+
+    socket.on('joinRoom', roomName => {
+        socket.join(roomName)
+        socket.on('message', msg => {
+            io.to(roomName).emit('message', msg)
+        })
+    })
+    socket.on('leaveRoom', roomName => {
+        socket.leave(roomName)
+    })
+})
